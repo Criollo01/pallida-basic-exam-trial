1b13ff6b7b0d7580b7352104fc6addf4adddead6
Criollo01 <lucanagy@protonmail.com>
2017-09-19 16:32:14 +0200

    feat(dictionary): first task done, second and last tasks are not

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -6,21 +6,25 @@ dictionary = [
 # Implement this method. It should add the given key-value pair to the
 # list 'dictionary'
 
-
-
 def add_word(hun_word, eng_word):
     dictionary.append({hun_word, eng_word})
     print(dictionary)
-    
+
 add_word("barack", "peach")
 
 # Implement these methods. They should return the translation of the given
 # word form the list 'dictionary'
 
-
 def translate_to_hun(eng_word):
-    pass
+
+
+translate_to_hun("apple")
 
 
 def translate_to_eng(hun_word):
     pass
+
+
+    # for key, value in dictionary.items():
+    #     if eng_word == value:
+    #         print(key)
