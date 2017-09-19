420b212ad6e2a91996a7bfe8a2c486f5947c4ff3
Criollo01 <lucanagy@protonmail.com>
2017-09-19 14:20:50 +0200

    feat(odd_filter): deleted all the previous solutions, left one, it's working

--- oddfilter/odd_filter.py
+++ oddfilter/odd_filter.py
@@ -4,18 +4,10 @@
 
 # print(odd_filter([1, 2, 3, 4, 5]))  # should print [1, 3, 5]
 
-numbers_list = [1, 2, 3, 4, 5]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# def odd_filter(numbers_list):
-#     for i in numbers_list:
-#         if numbers_list[i] % 2 == 1:
-#         return [numbers_list[0]]
-# print(numbers_list)
-
-# odd_filter(numbers_list)
-
-for i in range(0, len(numbers_list)):
-    odd_list = []
-    if numbers_list[i] % 2 != 0 :
-        odd_list.append(numbers_list[i])
+def odd_filter(numbers_list):
+    odd_list = [i for i in numbers_list if i % 2 != 0]
     print(odd_list)
+
+odd_filter(numbers)
